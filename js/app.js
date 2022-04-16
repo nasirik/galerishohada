807422194b262f589eeef2639805b8abd6ccaea7
nasirik <nasirimohammad313@gmail.com>
2022-04-17 00:27:01 +0430

    t123
    t123

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function showkashmar(cat){
+    document.getElementById("asemi").style.display = "none";
+    document.getElementById("yagmaei").style.display = "none";
+
+    const el= document.getElementById(cat);
+    el.style.display = "block";
+}
